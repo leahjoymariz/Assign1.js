c5939932cdd96562b8375263394391084080a051
leahjoymariz <121843074+leahjoymariz@users.noreply.github.com>
2023-01-30 00:32:41 -0500

    Add files via upload

---
+++ assign1.js
@@ -0,0 +1,19 @@
+console.log("Scriptassign");
+var userName= "Sleep Easy Relax Channel";
+var isSubscribed="True";
+var videoName="Instant Relief from Stress and Anxiety";
+var numberSubscribers="716,000";
+var numberViews="47,000,000";
+var numberDuration="3 hours";
+var yearUploaded="4 years ago";
+
+
+document.write(`
+    <p>Name: ${userName}</p>
+    <p>Subscribed: ${isSubscribed}</p>
+    <p>Video Name: ${videoName}</p>
+    <p>Subscribers: ${numberSubscribers}
+    <p>Views: ${numberViews}</p>
+    <p>Duration: ${numberDuration}</p>
+    <p>Uploaded: ${yearUploaded}</p>
+`)
